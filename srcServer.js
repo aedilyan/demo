ecf5a07f782e9867dad84639af66ff23deb5eacb
aedilyan <you@example.comaedilyan@gmail.com>
2019-04-09 18:27:12 +0400

    2

--- srcServer.js
+++ srcServer.js
@@ -2,17 +2,19 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import config from './webpack.config.dev';
 
 
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
-app.use(express.static(__dirname + '/dist'));
 
-app.use(require('webpack-dev-middleware')(compiler, {    
-    
-}));
+app.use(express.static('dist'));
+app.use(express.static('src'));
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    publicPath: config.output.publicPath
+}))
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,20 +1,58 @@
 import path from 'path';
 
 module.exports = {
-    mode: 'development',
+    // mode: 'development',
+    // devtool: 'inline-source-map',
+    // entry: {
+    //     app: path.resolve(__dirname, 'src/index.js'),
+    //     utils: path.resolve(__dirname, 'src/utils.js'),
+    // },
+    // output: {
+    //     filename: '[name].js',
+    //     path: __dirname + '/dist'
+    // },
+    // plugins: [],
+    // module: {
+    //     rules: [
+    //         { test: /\.css$/, use: 'css-loader' }
+    //     ]
+    // }
     devtool: 'inline-source-map',
-    entry: {
-        app: path.resolve(__dirname, 'src/index.js'),
-        utils: path.resolve(__dirname, 'src/utils.js'),
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/dist'
-    },
-    plugins: [],
-    module: {
-        rules: [
-            { test: /\.css$/, use: 'css-loader' }
-        ]
-    }
+ mode: 'development',
+ entry: [
+   'eventsource-polyfill', // necessary for hot reloading with IE
+   'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+   path.resolve(__dirname, 'src')
+ ],
+ target: 'web',
+ output: {
+   path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+   publicPath: '/',
+   filename: 'bundle.js'
+ },
+ devServer: {
+   contentBase: path.resolve(__dirname, 'src')
+ },
+ plugins: [
+   
+ ],
+ module: {
+   rules: [
+     {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       use: {
+         loader: 'babel-loader',
+         options: {
+           cacheDirectory: true
+         }
+       }
+     },
+    
+     {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+     {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+     {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+     {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+   ]
+ }
 };
