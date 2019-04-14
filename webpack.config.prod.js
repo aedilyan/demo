82746ec8785f0b674373385ad8a1fb09b2efcbeb
aedilyan <aedilyan@gmail.com>
2019-04-14 12:24:19 +0400

    css

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,9 @@ import path from 'path';
 // import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMD5Hash from 'webpack-md5-hash';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserJSPlugin from 'terser-webpack-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 
 export default {
     mode: 'production',
@@ -14,7 +17,12 @@ export default {
         filename: '[name].[chunkhash].js',
         path: __dirname + '/dist'
     },
-    plugins: [        
+    plugins: [
+        //Generate an external css file
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+        }),
         //hash the file using MD5 so that their name change when the code changes
         new WebpackMD5Hash(),
         new HtmlWebpackPlugin({
@@ -41,13 +49,13 @@ export default {
             {
                 test: /\.css$/,
                 use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true
-                    }
-                }]
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader',
+                ]
             }
         ]
     },
@@ -59,11 +67,12 @@ export default {
             automaticNameDelimiter: '~',
             name: true,
             cacheGroups: {
-              libs: {
-                test: /[\\/]node_modules[\\/]/,
-                priority: -10
-              }
+                libs: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                }
             }
-          }
+        },
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
     }
 };
