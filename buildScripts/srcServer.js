5133d5a43d04cdc52848001dee0ce9ccceb10ce3
aedilyan <aedilyan@gmail.com>
2019-04-24 13:28:45 +0400

    save

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -16,7 +16,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler))
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../dist/index2.html'));
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.get('/users', function (req, res) {
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,7 +10,8 @@ export default {
     },
     output: {
         filename: 'bundle.js',
-        path: __dirname + '/dist'
+        path: __dirname + '/dist',
+        publicPath: '/'
     },
     devServer: {
         hot: true
